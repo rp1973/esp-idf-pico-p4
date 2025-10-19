126b98a730bd15af7a59088e2b257a5115ba121c
Ralf Peters <ralf.peters@mur-peters.de>
2025-10-19 15:51:46 +0200

    cmake .

--- CMakeFiles/git-data/grabRef.cmake
+++ CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "/home/rpeters/Dokumente/Development/Projects/SDK-IDF/esp-idf-pico-p4/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "/home/rpeters/esp/esp-idf/.git")
+set(GIT_DIR "/home/rpeters/Dokumente/Development/Projects/SDK-IDF/esp-idf-pico-p4/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
