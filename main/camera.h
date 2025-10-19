6c1287077e21fe28d0c186fd9909b237fb81fcad
rp1973 <ralf.peters@mur-peters.de>
2025-10-19 13:52:15 +0200

    Dateinamen von piccture auf picture korrigieren

---
+++ main/camera.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_err.h"
+
+#include "camera_driver.h"
+#include "picture.h"
+#include "com.h"
+
+typedef struct {
+    encoder_handle_t encoder;
+    transport_handle_t transport;
+} camera_pipeline_t;
+
+camera_config_t camera_default_config(void);
+esp_err_t camera_driver_setup(const camera_config_t *config);
+esp_err_t camera_start_pipeline(camera_pipeline_t *pipeline);
+void camera_stop_pipeline(void);
---
+++ main/picture.c
@@ -0,0 +1,26 @@
+#include "picture.h"
+
+encoder_config_t piccture_default_config(void)
+{
+    return image_processing_default_encoder_config();
+}
+
+esp_err_t piccture_create_encoder(const encoder_config_t *config, encoder_handle_t *handle)
+{
+    return image_processing_create_encoder(config, handle);
+}
+
+void piccture_destroy_encoder(encoder_handle_t handle)
+{
+    image_processing_destroy_encoder(handle);
+}
+
+esp_err_t piccture_encode_frame(encoder_handle_t handle, const camera_frame_t *frame, h264_packet_t *packet)
+{
+    return image_processing_encode_frame(handle, frame, packet);
+}
+
+void piccture_release_packet(encoder_handle_t handle, h264_packet_t *packet)
+{
+    image_processing_release_packet(handle, packet);
+}
